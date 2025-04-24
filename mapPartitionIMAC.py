c152c42e462ae26980726a6686cc0f2ffc571fb6
Jason Ho <jason_ho@utexas.edu>
2025-04-24 11:25:27 -0500

    Updates which add in pos and neg weights
    More Updates. I think that I should probably make it so that the mappings map the full 32x32 array, even if nothing is happening...
    
    At the very least, I need to map to make everyone have at least 32 stuff and 1 bias. Otherwise, it will not make any sense I think?
    
    Maybe that is the compromise that I make. We use 32x1 with bias for everyone, and then go from there. At least at that point, it is the same architecture as the things that we model.

--- mapPartitionIMAC.py
+++ mapPartitionIMAC.py
@@ -5,6 +5,10 @@ import numpy as np
 import resource
 import os
 
+# FIXME: I think they do some fundamentally interesting stuff here...
+#        First off, they consider ideal op-amps.
+#        Second off, they don't actually have 32x32 partitions, but make their partitions such that they fit the actual guy, which is incorrect.
+
 # Quick Band Aid Fix to Make My Life a little easier :)
 # Increase the number of files I can open at a time 
 soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
@@ -16,6 +20,28 @@ resource.setrlimit(resource.RLIMIT_NOFILE, (min(20000, hard), hard))  # Change 4
 soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
 print(f"Soft limit: {soft}")
 print(f"Hard limit: {hard}")
+
+
+def find_partition(partitions, index):
+    """
+    Given a list of split indices and a target index,
+    returns (partition_number, index_within_partition).
+
+    # Note Indices are 1-indexed, and partitions are non-inclusive
+    # That is, 1:30 means that the partition is from 1 to 29
+    
+    Example:
+    partitions = [1, 30, 50]
+    index = 35
+    => (2, 6)
+    """
+    for i in range(len(partitions) - 1):
+        start = partitions[i]
+        end = partitions[i + 1]
+        if start <= index < end:
+            return i+1, (index - start) + 1
+    raise ValueError(f"Index {index} not found in any partition range.")
+
 
 def mapPartition(layer1,layer2, xbar_length, LayerNUM,hpar,vpar,metal,T,H,L,W,D,eps,rho,weight_var,data_dir,spice_dir): 
     # updating the resistivity for specific technology node
@@ -92,7 +118,162 @@ def mapPartition(layer1,layer2, xbar_length, LayerNUM,hpar,vpar,metal,T,H,L,W,D,
     print(f"Difference: {np.diff(vertical_cuts)}")
     print(f"Note: Each Vertical Guy gets own bias. Each Partition's initial wire connects to vdd")
 
+    # Open file descriptors for all possible things that we need to open (Each guy is xbar_length x 1) :)
+    # File descriptor named: partioned_layer_{layer_num}_{hpar}_{vpar}_{split_vpar}.sp
+    # We split vpar since we have 32x1 based mac units.
+    open_fd = {}    # Index into this using hpar, vpar, split_vpar index
 
+    for x_id in range(hpar):
+        for y_id in range(vpar):
+            # Vertical refers to having to split up input into multiple :)
+            # In our case, even though we will have the xbar_length x xbar_length, we will split to 32x1 for parallezability
+            # Get vertical cuts length :)
+            new_range_low = vertical_cuts[y_id]-1
+            new_range_high = vertical_cuts[y_id+1]-1
+
+
+            for split_vpar in range(new_range_low, new_range_high):
+                file_template = f"partitioned_layer_{LayerNUM}_{x_id+1}_{y_id+1}_{split_vpar+1}.sp"
+                fd = open(os.path.join(spice_dir,file_template), "w")
+
+                # Write subcircuit definition :)
+                fd.write(f".SUBCKT layer{LayerNUM}_{x_id+1}_{y_id+1}_{split_vpar+1}"+" vdd vss 0 ")
+
+                # Number of input and output in the circuit definition :)
+                for i in range(xbar_length):
+                    fd.write("in%d "%(i+1))
+                
+                fd.write("out%d"%(1))
+
+                fd.write("\n\n**********Positive Weighted Array**********\n")
+
+                open_fd[(x_id+1, y_id+1, split_vpar+1)] = fd    
+
+    
+    # Open all the weights and bias files :)
+    posw_r=open(data_dir+'/'+'posweight'+str(LayerNUM)+".txt", "r") # read the positive line conductances
+    negw_r=open(data_dir+'/'+'negweight'+str(LayerNUM)+".txt", "r")
+    posb_r=open(data_dir+'/'+'posbias'+str(LayerNUM)+".txt", "r")
+    negb_r=open(data_dir+'/'+'negbias'+str(LayerNUM)+".txt", "r")
+
+
+    # Go through and write each of the files :)
+    # TODO:
+
+
+    # Write Positive Array 
+    # FIXME: Last horizontal partition still needs the bias :)
+    n_hpar=1 # horizontal partition number
+    c=1 # column number
+    r=1 # row number
+    for line in posw_r:
+        if (float(line)!=0):
+            if (r < layer2+1):
+                # Calculate Indices :) (Row is vertical partitioning, while column is vertical partitioning)
+                y_id, split_r = find_partition(vertical_cuts, r)
+                x_id, split_c = find_partition(horizontal_cuts, c)
+                #print(f"Row: {r}, Col: {c}, X_ID: {x_id}, Y_ID: {y_id}, X_PAR: {split_c}, Y_PAR: {split_r}")
+                open_fd[(x_id, y_id, split_r)].write("Rwpos%d_%d in%d_%d sp%d_%d %f\n"% (split_c,split_r, split_c,split_r,split_c,split_r,float(line)))
+
+                #layer_w.write("Rwpos%d_%d_%d in%d_%d sp%d_%d %f\n"% (c,r, n_hpar, c,r,c,r,float(line)))
+                r+=1;
+            else:
+                c+=1;
+                r=1;
+                if (c == int(layer1_wb*n_hpar/hpar+min((layer1_wb%hpar)/n_hpar,1)+1)):
+                    print("positive increase horizontal partition")
+                    print(f"row: {r}, col: {c}")
+                    n_hpar+=1
+
+                # Calculate Indices :) (Row is vertical partitioning, while column is vertical partitioning)
+                y_id, split_r = find_partition(vertical_cuts, r)
+                x_id, split_c = find_partition(horizontal_cuts, c)
+
+                #print(f"Row: {r}, Col: {c}, X_ID: {x_id}, Y_ID: {y_id}, X_PAR: {split_c}, Y_PAR: {split_r}")
+
+                open_fd[(x_id, y_id, split_r)].write("Rwpos%d_%d in%d_%d sp%d_%d %f\n"% (split_c,split_r, split_c,split_r,split_c,split_r,float(line)))
+                #layer_w.write("Rwpos%d_%d_%d in%d_%d sp%d_%d %f\n"% (c,r,n_hpar, c,r,c,r,float(line)))
+                r+=1;
+        else:
+            r+=1;
+
+    # Write Negative Array
+    for key, file in open_fd.items():
+        try:
+            file.write("\n\n**********Negative Weighted Array**********\n")
+
+        except Exception as e:
+            print(f"Error {key}: {e}")
+            
+    n_hpar=1 # horizontal partition number
+    c=1 # column number
+    r=1 # row number
+    for line in negw_r:
+        if (float(line)!=0):
+            if (r < layer2+1):
+                y_id, split_r = find_partition(vertical_cuts, r)
+                x_id, split_c = find_partition(horizontal_cuts, c)
+                #print(f"Row: {r}, Col: {c}, X_ID: {x_id}, Y_ID: {y_id}, X_PAR: {split_c}, Y_PAR: {split_r}")
+                open_fd[(x_id, y_id, split_r)].write("Rwneg%d_%d in%d_%d sn%d_%d %f\n"% (split_c,split_r,split_c,split_r,split_c,split_r,float(line)))
+
+                #layer_w.write("Rwneg%d_%d in%d_%d sn%d_%d %f\n"% (c,r,c,r,c,r,float(line)))
+                r+=1;
+            else:
+                c+=1;
+                r=1;
+                if (c == int(layer1_wb*n_hpar/hpar+min((layer1_wb%hpar)/n_hpar,1)+1)):
+                    n_hpar+=1
+
+                y_id, split_r = find_partition(vertical_cuts, r)
+                x_id, split_c = find_partition(horizontal_cuts, c)
+                #print(f"Row: {r}, Col: {c}, X_ID: {x_id}, Y_ID: {y_id}, X_PAR: {split_c}, Y_PAR: {split_r}")
+                open_fd[(x_id, y_id, split_r)].write("Rwneg%d_%d in%d_%d sn%d_%d %f\n"% (split_c,split_r,split_c,split_r,split_c,split_r,float(line)))
+
+
+                #layer_w.write("Rwneg%d_%d in%d_%d sn%d_%d %f\n"% (c,r,c,r,c,r,float(line)))
+                r+=1;
+        else:
+            r+=1;	
+    
+
+    # Write Bias (if applicable)
+    # writing the circuit for positive line biases
+    # This only applies for the last layer :)
+    # TODO: Start off here on the biases :)
+    layer_w.write("\n\n**********Positive Biases**********\n\n")
+    r=1
+    for line in posb_r:
+        if (float(line)!=0):
+            layer_w.write("Rbpos%d vd%d sp%d_%d %f\n"% (r,r,horizontal_cuts[-1],r,float(line)))
+            r+=1
+        else:
+            r+=1
+
+
+    # Write 
+
+
+
+
+
+
+    # Close All File Descriptors :)
+    posw_r.close()
+    negw_r.close()
+    posb_r.close()
+    negb_r.close()
+
+
+    # TODO: Close all other file descriptors
+    # Close all file descriptors
+    for key, file in open_fd.items():
+        try:
+            file.close()
+            print(f"Closed: {key}")
+        except Exception as e:
+            print(f"Error closing {key}: {e}")
+
+            
 
 
 
@@ -119,61 +300,7 @@ def mapPartition(layer1,layer2, xbar_length, LayerNUM,hpar,vpar,metal,T,H,L,W,D,
 
 
 
-
-
-
-
-
-
-
-
-    # Open file descriptors for all possible things that we need to open (Each guy is xbar_length x 1) :)
-    # File descriptor named: partioned_layer_{layer_num}_{hpar}_{vpar}_{split_vpar}.sp
-    # We split vpar since we have 32x1 based mac units.
-    open_fd = {}    # Index into this using hpar, vpar, split_vpar index
-
-    for x_id in range(hpar):
-        for y_id in range(vpar):
-            # Vertical refers to having to split up input into multiple :)
-            # In our case, even though we will have the xbar_length x xbar_length, we will split to 32x1 for parallezability
-            for split_vpar in range(xbar_length):
-                file_template = f"partitioned_layer_{LayerNUM}_{x_id+1}_{y_id+1}_{split_vpar+1}.sp"
-                fd = open(os.path.join(spice_dir,file_template), "w")
-
-                # Write subcircuit definition :)
-                fd.write(".SUBCKT layer"+ str(LayerNUM)+" vdd vss 0 ")
-
-                # Number of input and output in the circuit definition :)
-                for i in range(xbar_length):
-                    fd.write("in%d "%(i+1))
                 
-                fd.write("out%d \n"%(i+1))
-
-                open_fd[(x_id+1, y_id+1, split_vpar+1)] = fd    
-
-    # Open all the weights and bias files :)
-    posw_r=open(data_dir+'/'+'posweight'+str(LayerNUM)+".txt", "r") # read the positive line conductances
-    negw_r=open(data_dir+'/'+'negweight'+str(LayerNUM)+".txt", "r")
-    posb_r=open(data_dir+'/'+'posbias'+str(LayerNUM)+".txt", "r")
-    negb_r=open(data_dir+'/'+'negbias'+str(LayerNUM)+".txt", "r")
-
-
-    # Go through and write each of the files :)
-
-
-
-
-
-
-
-
-    # Close All File Descriptors :)
-    posw_r.close()
-    negw_r.close()
-    posb_r.close()
-    negb_r.close()
-    # TODO:
-
 
 
 
@@ -342,5 +469,5 @@ def mapPartition(layer1,layer2, xbar_length, LayerNUM,hpar,vpar,metal,T,H,L,W,D,
     layer_w.close()
 
 # Test Function
+mapPartition(84, 10, 32, 3, 3, 1, 2.69999999997e-8, 2.2e-8, 2e-8, 1.35e-7, 1.08e-7, 4.5e-8, 1.77079999999e-10, 1.9e-8, 0, 'data', 'test_spice')
 #Horizontal is 3, vertical is 1
-mapPartition(120, 84, 32, 2, 4, 3, 2.69999999997e-8, 2.2e-8, 2e-8, 1.35e-7, 1.08e-7, 4.5e-8, 1.77079999999e-10, 1.9e-8, 0, 'data', 'test_spice')
